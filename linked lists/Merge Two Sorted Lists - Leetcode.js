dc4b66e22adea9b940057fa4bbe0956931c8d069
Daniel Moreno <159810372+danielmorenotx@users.noreply.github.com>
2024-07-01 16:32:26 -0500

    Update Merge Two Sorted Lists - Leetcode.js

--- linked lists/Merge Two Sorted Lists - Leetcode.js
+++ linked lists/Merge Two Sorted Lists - Leetcode.js
@@ -17,3 +17,34 @@ Return the head of the merged linked list.
  * @return {ListNode}
  */
 
+var mergeTwoLists = function(list1, list2) {
+
+    // a linked list value will be the head of the list
+
+    let dummy = new ListNode(); // creates a starting point for the linked lists;
+    // because no values are passed, val === 0 and next === null;
+    let current = dummy; // keep track of the merged lists, will be returned
+
+    // check if the two lists are empty
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) { // checks if starting value of list is bigger in list 1 or 2
+            current.next = list1; // if so, sets the current to the value of list2
+            list1 = list1.next; //
+        } else { // checks if they are the same or if list1.val is bigger
+            current.next = list2; // now pointing to list2
+            // linked list traversal
+            list2 = list2.next; // moves the variable of the list2 to the next variable in the list
+        }
+        current = current.next;
+    }
+
+    // If one of the lists is not empty, append it
+    if (list1 !== null) {
+        current.next = list1;
+    } else if (list2 !== null) {
+        current.next = list2;
+    }
+
+    // The dummy node's next points to the head of the merged list
+    return dummy.next; // returning the originally created placeholder
+};
