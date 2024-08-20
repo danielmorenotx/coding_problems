f16599c1def4b608a4f0086bbaa09a94c8baa9c3
Daniel Moreno <159810372+danielmorenotx@users.noreply.github.com>
2024-08-20 10:05:13 -0500

    Update and rename Find Smallest Letter Greater Than Target - Leetcode.java to 744. Find Smallest Letter Greater Than Target - Leetcode.java

--- LeetCode/744. Find Smallest Letter Greater Than Target - Leetcode.java
+++ LeetCode/744. Find Smallest Letter Greater Than Target - Leetcode.java
@@ -1,5 +1,5 @@
+// https://leetcode.com/problems/find-smallest-letter-greater-than-target
 // You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
-
 // Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
 
 class Solution {
