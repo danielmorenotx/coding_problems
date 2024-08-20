d01219dc7a1b00cbc09878426085c6629653085f
Daniel Moreno <159810372+danielmorenotx@users.noreply.github.com>
2024-08-20 15:11:56 -0500

    Update 3110. Score of a String.js

--- LeetCode/3110. Score of a String.js
+++ LeetCode/3110. Score of a String.js
@@ -1,4 +1,6 @@
 /**
+https://leetcode.com/problems/score-of-a-string/description/
+
 You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
 
 Return the score of s.
